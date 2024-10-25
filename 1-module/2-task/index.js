5c9b084cf3d5a8da8d0d31f029e87255d6f4ebb0
fatima <botashevaf6798@gmail.com>
2024-10-25 07:22:28 +0200

    add checkSpam task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if(!name) return name && name.length >4 // ваш код...
+  return !name ===null && !name.includes(' ')  && name.length >=4 // ваш код...
 }
 
 function sayHello() {
