3b329189c9f040b76f127bac5c4fa62e628014b3
fatima <botashevaf6798@gmail.com>
2024-11-02 15:20:21 +0100

    add task of salary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
+
 function sumSalary(salaries) {
+
+  let sumSalary =0;
+
+  for(let key in salaries){
+    if(salaries[key] && typeof salaries[key]==='number'){
+      sumSalary += salaries[key];
+  }
+}
+return sumSalary
   // ваш код...
 }
+console.log(sumSalary(salaries))
